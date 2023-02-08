b4c0fd9ce7efb43c12a3cd2a8424517e44cf3033
Alexandr Kabanau <Azraill1989@gmail.com>
2023-02-08 17:39:27 +0300

    feat: create simple earth, add capitals, click on it? and basick layout

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,28 +1,62 @@
 // import { generateHeader } from "../../components/header/header";
 
-export function generateHomePage(): string {
-  return `
+export function generateHomePage() {
+  const db = {
+    '1': 'Australia',
+    '2': 'Brazil',
+    '3': 'China',
+    '4': 'Russia',
+    '5': 'USA',
+  };
+  const div: HTMLElement = document.createElement("div");
+  div.className = "d-grid gap-2";
+  div.role = "group";
+  div.ariaLabel = "Vertical button group";
+  let prop: keyof typeof db
+  for (prop in db) {
+    // console.log(`db.${prop} = ${db[prop]}`);
+    const button: HTMLButtonElement = document.createElement("button");
+    button.type = "button";
+    button.className = "btn btn-primary";
+    button.innerText = db[prop];
+    div.append(button);
+  }
+  const getString = (el:HTMLElement) => el.outerHTML;
+  const content = document.createElement("div");
+  // content.className = "position-relative";
+  content.innerHTML = `
   <canvas class="home-page"></canvas>
-  <div class=" container py-5 text-center">
-    <div class="col-md-1">
-      <button type="button" class="button help-button">
-        <img src="./assets/icons/light_theme_white.svg" alt="help">
-      </button>
-    </div>
-    <div class="col-md-9">
-      <h3 class="display-1 fw-semibold" data-18i="titleChoice">
-        Choose the country
-      </h3>
-    </div>
-    <div class="col-md-2">
-      <select class="form-select" aria-label="Default select example">
-        <option value="1" selected>EN</option>
-        <option value="2">RU</option>
-        <option value="3">BE</option>
-      </select>
+  <div class="container py-2 text-center">
+    <div class="home-page__container">
+      <div class="row ">
+        <div class="col-2">
+          ${getString(div)}
+        </div>
+        <div class="col-7">
+          <h4 class="home-page__title display-2 fw-semibold" data-18i="titleChoice">
+            Choose the country
+          </h4>
+        </div>
+        <div class="col-2">
+          <select class="form-select" aria-label="Default select example">
+            <option value="1" selected>EN</option>
+            <option value="2">RU</option>
+            <option value="3">BE</option>
+          </select>
+        </div>
+        <div class="col-1">
+          <button type="button" class="button theme-button">
+            <img src="./assets/icons/light_theme_white.svg" alt="help">
+          </button>
+        </div>
+      </div>
     </div>
   </div>
-  `
+  `;
+  //add handlers
+
+  return content;
+
   
   
   
--- src/router.ts
+++ src/router.ts
@@ -3,14 +3,9 @@ import { generateHeader } from "./components/header/header";
 import { generateCountryPage } from "./pages/country/country";
 import { generateHelpPage } from "./pages/help/help";
 import { generateStartPage } from "./pages/start/start";
-import { sceneInitStartPage } from "./pages/start/sceneInit";
-<<<<<<< HEAD
-import { generateHeader } from "./components/header/header";
+import { sceneInitStartPage } from "./components/canvas/SceneInit";
 import { generateError404Page } from "./pages/error404/error404";
-=======
-import { startPageHandlers } from "./pages/start/handlers";
 import { generateHomePage } from "./pages/home/home";
->>>>>>> 2e28adf (add and remove buttoms implement)
 
 export function router() {
   generateContentByHash();
@@ -22,14 +17,9 @@ export function router() {
 function generateContentByHash() {
   const main = document.querySelector('.root') as HTMLElement;
   const header = document.querySelector('.header') as HTMLElement;
-<<<<<<< HEAD
   let contentHeader = generateHeader();
   main.innerHTML = '';
   let contentMain = null;
-=======
-  let contentMain = '';
-  let contentHeader = generateHeader(); // Вставить функцию генерации хедера
->>>>>>> 2e28adf (add and remove buttoms implement)
   const [hash, id] = window.location.hash.slice(1).split('/');
   switch (hash) {
     case ROUTER_PATH.HELP: contentMain = generateHelpPage();
@@ -46,22 +36,14 @@ function generateContentByHash() {
     case ROUTER_PATH.START: {
       contentMain = generateStartPage();
       contentHeader = '';
-      setTimeout(() => {
-        sceneInitStartPage();
-        startPageHandlers();// I really dont know how to use it another way
-      }, 0);
       break;
     }
     default: contentMain = generateError404Page();
   }
   header.innerHTML = contentHeader;
-<<<<<<< HEAD
   main.append(contentMain);
 
   if (hash === ROUTER_PATH.START || hash === '') {
     sceneInitStartPage();
   };
-=======
-  main.innerHTML = contentMain;
->>>>>>> 2e28adf (add and remove buttoms implement)
 }
