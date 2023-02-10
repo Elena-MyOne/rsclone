9c343ea564171fb0d6d9dcc07b699c2fd3d107f7
Alexandr Kabanau <Azraill1989@gmail.com>
2023-02-10 15:42:21 +0300

    feat: create planet dark mode

--- src/components/canvas/SceneInit.ts
+++ src/components/canvas/SceneInit.ts
@@ -177,13 +177,11 @@ scene.add(galaxy);
 }
 
 export function sceneInitHomePage() {
-  // console.log(1);
-
-
+  
   //Canvas
   const canvas = document.querySelector('canvas.home-page') as HTMLElement;
-  // console.log(canvas)
   let topOffset = Number(canvas.offsetTop);
+  
   // Scene
   const scene = new THREE.Scene();
 
@@ -192,7 +190,6 @@ export function sceneInitHomePage() {
     width: window.innerWidth,
     height: document.body.clientHeight - topOffset
   }
-
 
   window.addEventListener('resize', () =>
   {
@@ -209,9 +206,6 @@ export function sceneInitHomePage() {
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
   })
-  // window.addEventListener('scroll', () => {
-  //   topOffset = Number(canvas.offsetTop);
-  // })
 
   // Base camera
   const camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height, 0.1, 1000);
@@ -232,9 +226,9 @@ export function sceneInitHomePage() {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
   renderer.autoClear = false;
   renderer.setClearColor(0x000000, 0.0);
-
-
-  //loading console.log - debug
+  
+  
+  // loading
   const loadingManager = new THREE.LoadingManager()
   loadingManager.onStart = () =>
   {
@@ -253,49 +247,30 @@ export function sceneInitHomePage() {
       console.log('loadingManager: loading error')
   }
   const textureLoader = new THREE.TextureLoader(loadingManager);
-  const colorTexture = textureLoader.load(
-    '../../assets/images/textures/2_no_clouds_8k-min.jpg'
-  )
-  const bumpTexture = textureLoader.load(
-    '../../assets/images/textures/elev_bump_8k-min.jpg'
-  )
-  const borderTexture = textureLoader.load(
-    '../../assets/images/textures/boundaries_8k.png'
-  )
+
+  const earth = createPlanet();
+
+  scene.add(earth);
+  
+  // galaxy geometry
+  const starGeometry = new THREE.SphereGeometry(80, 64, 64);
+
+  // galaxy material
   const galaxyTexture = textureLoader.load(
     '../../assets/images/textures/galaxy.png'
   )
-  function createEarth(colorTexture: THREE.Texture, bumpTexture: THREE.Texture) {
-    const earthGeometry = new THREE.SphereGeometry(1, 32, 32);
-    const earthMaterial = new THREE.MeshStandardMaterial({
-        metalness: 1,
-        map: colorTexture,
-        bumpMap: bumpTexture,
-        bumpScale: 0.005,
-        metalnessMap: borderTexture,
-    });
-    return new THREE.Mesh(earthGeometry, earthMaterial);
-  }
-  const earth = createEarth(colorTexture, bumpTexture);
-  scene.add(earth);
+  const starMaterial = new THREE.MeshBasicMaterial({
+      map : galaxyTexture,
+      side: THREE.BackSide
+  });
 
-  function createGalaxy() {
-    // galaxy geometry
-    const starGeometry = new THREE.SphereGeometry(80, 64, 64);
+  // galaxy mesh
+  const galaxy = new THREE.Mesh(starGeometry, starMaterial);
 
-    // galaxy material
-    const starMaterial = new THREE.MeshBasicMaterial({
-        map : galaxyTexture,
-        side: THREE.BackSide
-    });
-
-    // galaxy mesh
-    return new THREE.Mesh(starGeometry, starMaterial);
-}
-  const galaxy = createGalaxy();
   scene.add(galaxy);
 
-  const ambientlight = new THREE.AmbientLight(0x333333, 1); // 5 for night
+  //ambient light
+  const ambientlight = createAmbientLight();
   scene.add(ambientlight);
 
   // point light
@@ -343,11 +318,7 @@ export function sceneInitHomePage() {
   const arrOfPins: Array<THREE.Mesh<THREE.SphereGeometry, THREE.MeshBasicMaterial>> = [];
   for (let city of cities) {
     const cityCoordinates = calcPosFromLatLonRad(city.lat, city.lon);
-    const pin = new THREE.Mesh(
-      new THREE.SphereGeometry(0.035, 10, 10),
-      new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-    )
-    // console.log(cityCoordinates)
+    const pin = createPin();
     pin.name = city.name;
     arrOfPins.push(pin);
     pin.position.set(cityCoordinates.x, cityCoordinates.y, cityCoordinates.z);
@@ -356,14 +327,7 @@ export function sceneInitHomePage() {
 
   //Raycaster
   const raycaster = new THREE.Raycaster();
-  // let intersects: THREE.Intersection[];
   let intersectedObject: THREE.Object3D | null;
-  const originalMaterials: { [id: string]: THREE.Material | THREE.Material[] } =
-    {}
-  const highlightedMaterial = new THREE.MeshBasicMaterial({
-    wireframe: true,
-    color: 0x00ff00
-  })
 
 
   //handlers
@@ -410,118 +374,120 @@ export function sceneInitHomePage() {
   }
 
   tick()
-  // console.log(scene);
 }
 
+const createPlanet = () => {
+  const loadingManager = new THREE.LoadingManager()
+  loadingManager.onStart = () =>
+  {
+      console.log('loadingManager: loading started')
+  }
+  loadingManager.onLoad = () =>
+  {
+      console.log('loadingManager: loading finished')
+  }
+  loadingManager.onProgress = () =>
+  {
+      console.log('loadingManager: loading progressing')
+  }
+  loadingManager.onError = () =>
+  {
+      console.log('loadingManager: loading error')
+  }
+  const textureLoader = new THREE.TextureLoader(loadingManager);
+  const bumpTexture = textureLoader.load(
+    '../../assets/images/textures/elev_bump_8k-min.jpg'
+  );
+  const borderTexture = textureLoader.load(
+    '../../assets/images/textures/boundaries_8k.png'
+  );
+  const galaxyTexture = textureLoader.load(
+    '../../assets/images/textures/galaxy.png'
+  );
+  let earth: THREE.Mesh<THREE.SphereGeometry, THREE.MeshStandardMaterial>;
+  const theme = localStorage.getItem("theme") as string;
+  switch (theme) {
+    case "dark":
+      earth = createDarkPlanet(bumpTexture, borderTexture);
+      break;
 
+    default:
+      earth = createLightPlanet(bumpTexture, borderTexture);
+      break;
+  }
+  return earth;
+}
 
+const createDarkPlanet = (bumpTexture: THREE.Texture, borderTexture: THREE.Texture) => {
+  const loadingManager = new THREE.LoadingManager();
+  const textureLoader = new THREE.TextureLoader(loadingManager);
 
-
-// import * as THREE from 'three';
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
-// export default class SceneInit {
-//   scene: THREE.Scene;
-//   camera: THREE.PerspectiveCamera;
-//   renderer: THREE.WebGLRenderer;
-//   fov: number;
-//   nearPlane: number;
-//   farPlane: number;
-//   canvasClassName: string
-//   clock: THREE.Clock;
-//   controls: OrbitControls;
-//   ambientLight: THREE.AmbientLight;
-//   directionalLight: THREE.DirectionalLight;
+  const colorTexture = textureLoader.load(
+    '../../assets/images/textures/earth_nightlights_10K.jpg'
+  )
+  const earthGeometry = new THREE.SphereGeometry(1, 32, 32);
+  const earthMaterial = new THREE.MeshStandardMaterial({
+    color: 0xFFC489,
+    metalness: 1,
+    map: colorTexture,
+    bumpMap: bumpTexture,
+    bumpScale: 0.005,
+    metalnessMap: borderTexture,
+  });
+  return new THREE.Mesh(earthGeometry, earthMaterial);
   
-//   constructor(canvasClassName: string) {
-//     // NOTE: Core components to initialize Three.js app.
-//     this.scene = new THREE.Scene();
-//     this.camera = new THREE.PerspectiveCamera();
-//     this.renderer = new THREE.WebGLRenderer();
+}
 
-//     // NOTE: Camera params;
-//     this.fov = 45;
-//     this.nearPlane = 1;
-//     this.farPlane = 1000;
-//     this.canvasClassName = canvasClassName;
+const createLightPlanet = (bumpTexture: THREE.Texture, borderTexture: THREE.Texture) => {
+  const loadingManager = new THREE.LoadingManager();
+  const textureLoader = new THREE.TextureLoader(loadingManager);
 
-//     // NOTE: Additional components.
-//     this.clock = new THREE.Clock;
-//     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+  const colorTexture = textureLoader.load(
+    '../../assets/images/textures/2_no_clouds_8k-min.jpg'
+  )
+  const earthGeometry = new THREE.SphereGeometry(1, 32, 32);
+  const earthMaterial = new THREE.MeshStandardMaterial({
+    metalness: 1,
+    map: colorTexture,
+    bumpMap: bumpTexture,
+    bumpScale: 0.005,
+    metalnessMap: borderTexture,
+  });
+  return new THREE.Mesh(earthGeometry, earthMaterial);
+}
 
-//     // NOTE: Lighting is basically required.
-//     this.ambientLight = new THREE.AmbientLight();
-//     this.directionalLight = new THREE.DirectionalLight();
-//   }
+const createAmbientLight = () => {
+  let light: THREE.AmbientLight;
+  const theme = localStorage.getItem("theme") as string;
+  switch (theme) {
+    case "dark":
+      light = new THREE.AmbientLight(0x333333, 5);
+      break;
 
-//   initialize() {
-//     this.scene = new THREE.Scene();
-//     this.camera = new THREE.PerspectiveCamera(
-//       this.fov,
-//       window.innerWidth / window.innerHeight,
-//       1,
-//       1000
-//     );
-//     this.camera.position.z = 48;
+    default:
+      light = new THREE.AmbientLight(0x333333, 1);
+      break;
+  }
+  return light;
+}
 
-//     // NOTE: Specify a canvas which is already created in the HTML.
-//     const canvas = document.querySelector(this.canvasClassName) as HTMLElement;
-//     this.renderer = new THREE.WebGLRenderer({
-//       canvas,
-//       // NOTE: Anti-aliasing smooths out the edges.
-//       antialias: true,
-//     });
-//     this.renderer.setClearColor(0xff0, 0.0);
-//     this.renderer.setSize(window.innerWidth, window.innerHeight);
-//     // this.renderer.shadowMap.enabled = true;
-//     document.body.appendChild(this.renderer.domElement);
+const createPin = () => {
+  let pin: THREE.Mesh<THREE.SphereGeometry, THREE.MeshBasicMaterial>;
+  const theme = localStorage.getItem("theme") as string;
+  switch (theme) {
+    case "dark":
+      pin = pin = new THREE.Mesh(
+          new THREE.SphereGeometry(0.035, 10, 10),
+          new THREE.MeshBasicMaterial({ color: 0xFFC489 })
+        );
+      break;
 
-//     this.clock = new THREE.Clock();
-//     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-
-//     // ambient light which is for the whole scene
-//     this.ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
-//     this.ambientLight.castShadow = true;
-//     this.scene.add(this.ambientLight);
-
-//     // directional light - parallel sun rays
-//     this.directionalLight = new THREE.DirectionalLight(0xffffff, 2);
-//     // this.directionalLight.castShadow = true;
-//     this.directionalLight.position.set(0, 32, 64);
-//     this.scene.add(this.directionalLight);
-
-//     // if window resizes
-//     window.addEventListener('resize', () => this.onWindowResize(), false);
-
-//     // NOTE: Load space background.
-//     // this.loader = new THREE.TextureLoader();
-//     // this.scene.background = this.loader.load('./pics/space.jpeg');
-
-//     // NOTE: Declare uniforms to pass into glsl shaders.
-//     // this.uniforms = {
-//     //   u_time: { type: 'f', value: 1.0 },
-//     //   colorB: { type: 'vec3', value: new THREE.Color(0xfff000) },
-//     //   colorA: { type: 'vec3', value: new THREE.Color(0xffffff) },
-//     // };
-//   }
-
-//   animate() {
-//     // NOTE: Window is implied.
-//     // requestAnimationFrame(this.animate.bind(this));
-//     window.requestAnimationFrame(this.animate.bind(this));
-//     this.render();
-//     this.controls.update();
-//   }
-
-//   render() {
-//     // NOTE: Update uniform data on each render.
-//     // this.uniforms.u_time.value += this.clock.getDelta();
-//     this.renderer.render(this.scene, this.camera);
-//   }
-
-//   onWindowResize() {
-//     this.camera.aspect = window.innerWidth / window.innerHeight;
-//     this.camera.updateProjectionMatrix();
-//     this.renderer.setSize(window.innerWidth, window.innerHeight);
-//   }
-// }
+    default:
+      pin = new THREE.Mesh(
+          new THREE.SphereGeometry(0.035, 10, 10),
+          new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+        )
+      break;
+  }
+  return pin;
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,14 +9,15 @@ export function generateHomePage() {
  
   const root = document.querySelector('.root') as HTMLElement;
   root.classList.add('main_home-page');
+  root.dataset.dsTheme = 'dark';
   const content = document.createElement("div");
   // content.className = "position-relative";
   content.innerHTML = `
   <canvas class="home-page"></canvas>
   <div class="container py-2 text-center">
     <div class="home-page__container container">
-      <div class="row ">
-        <div class="col-2 country-buttons_container">
+      <div class="row">
+        <div class="col-2 country-buttons_container" data-bs-theme="dark">
         </div>
         <div class="col-7">
           <h4 class="home-page__title display-2 fw-semibold" data-18i="titleChoice">
