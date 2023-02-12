06980d5814b796f93b4e2c570cffabe4b19a0840
Alexandr Kabanau <Azraill1989@gmail.com>
2023-02-12 18:32:28 +0300

    fix: fix background for navBar offcanvas

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,38 +1,41 @@
 export function generateHeader(): string {
   return `
-      <header class="header">
-      <nav class="header__nav navbar navbar-expand-lg navbar-dark">
+      <nav class="header__nav navbar navbar-expand-lg">
         <div class="header__container container">
           <a class="header__logo navbar-brand" href="#home">Amazing Trip</a>
           <button
             class="navbar-toggler"
             type="button"
-            data-bs-toggle="collapse"
+            data-bs-toggle="offcanvas"
             data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
+            aria-controls="offcanvasDarkNavbar"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a data-i18="btnHome" class="nav-link active" aria-current="page" href="#home">Home</a>
-              </li>
-              <li class="nav-item">
-                <a data-i18="btnSignUp" class="nav-link" href="#registration">Sign up</a>
-              </li>
-              <li class="nav-item">
-                <a data-i18="btnLogIn" class="nav-link" href="#profile">Log in</a>
-              </li>
-              <li class="nav-item">
-                <a data-i18="btnHelp" class="nav-link" href="#help">Help</a>
-              </li>
-            </ul>
+          <div class="offcanvas offcanvas-top" id="navbarNav">
+            <div class="offcanvas-header">
+              <a class="header__logo navbar-brand" href="#home">Amazing Trip</a>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a data-i18="btnHome" class="nav-link active" aria-current="page" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a data-i18="btnSignUp" class="nav-link" href="#registration">Sign up</a>
+                </li>
+                <li class="nav-item">
+                  <a data-i18="btnLogIn" class="nav-link" href="#profile">Log in</a>
+                </li>
+                <li class="nav-item">
+                  <a data-i18="btnHelp" class="nav-link" href="#help">Help</a>
+                </li>
+              </ul>
+            <div>
           </div>
         </div>
       </nav>
-    </header>
   `
 }
