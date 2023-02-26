991bf323fd7cf697fd918c096b073875c174385d
Elena <myone72@yahoo.com>
2023-02-26 17:31:39 -0500

    feat: set change active link for header

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,5 +1,3 @@
-import { content } from "../../constants/i18n";
-
 export function generateHeader(): string {
 
   return `
@@ -26,7 +24,7 @@ export function generateHeader(): string {
             <div class="offcanvas-body">
               <ul class="navbar-nav">
                 <li class="nav-item">
-                  <a data-i18="btnHome" class="nav-link active" aria-current="page" href="#home">Home</a>
+                  <a data-i18="btnHome" class="nav-link" href="#home">Home</a>
                 </li>
                 <li class="nav-item nav-item-signup">
                   <a data-i18="btnSignUp" class="nav-link" href="#registration">Sign up</a>
